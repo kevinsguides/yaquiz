bf014f38ac2a456f92441c81282f09923372669f
Kevin Olson <kevin@kevinsguides.com>
2023-04-04 21:57:15 -0400

    quiz view results sort of works need to fix things to make more elegant

--- components/com_yaquiz/src/Model/UserModel.php
+++ components/com_yaquiz/src/Model/UserModel.php
@@ -1,8 +1,10 @@
 <?php
 namespace KevinsGuides\Component\Yaquiz\Site\Model;
+use Joomla\CMS\Log\Log;
 defined ('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\BaseModel;
+
 
 class UserModel extends BaseModel
 {
@@ -41,6 +43,36 @@ class UserModel extends BaseModel
         $db->setQuery($query);
         $results = $db->loadObjectList();
         return $results;
+    }
+
+    public function getIndividualResult($pk = null){
+
+        $app = Factory::getApplication();
+        $input = $app->input;
+
+        if($pk == null){
+            $pk = $input->get('resultid', null, 'INT');
+        }
+
+        Log::add('Loading result: ' . $pk, Log::INFO, 'yaquiz');
+
+        $db = Factory::getContainer()->get('DatabaseDriver');
+        $query = $db->getQuery(true);
+        $query->select('*');
+        $query->from($db->quoteName('#__com_yaquiz_results'));
+        $query->where($db->quoteName('id') . ' = ' . $db->quote($pk));
+        $db->setQuery($query);
+        $result = $db->loadObject();
+
+        //the result->user_id must be the same as the current user
+        $user = Factory::getApplication()->getIdentity();
+        if($result->user_id != $user->id){
+            $app->enqueueMessage('You do not have permission to view this result.', 'error');
+            return false;
+        }
+
+        return $result;
+
     }
 
 
