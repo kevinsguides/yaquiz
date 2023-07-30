3bd29ba7ee449f0f94461c8755bbc635befbaa7d
Kevin Olson <kevin@kevinsguides.com>
2023-07-30 11:54:24 -0400

    fix this

--- administrator/components/com_yaquiz/src/Controller/ScriptActionController.php
+++ administrator/components/com_yaquiz/src/Controller/ScriptActionController.php
@@ -5,39 +5,17 @@ defined ( '_JEXEC' ) or die ();
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Log\Log;
-use Joomla\CMS\Language\Text;
 
+use Joomla\CMS\Response\JsonResponse as JResponseJson;
 use Joomla\CMS\MVC\Controller\BaseController;
-use KevinsGuides\Component\Yaquiz\Administrator\Model\YaquizModel;
+
 
 class ScriptActionController extends BaseController {
 
 
-    public function saveQuestionOrdering() {
+    public function doSomething() {
 
-        //user has edit permissions
-        $user = Factory::getApplication()->getIdentity();
-        if (!$user->authorise('core.edit', 'com_yaquiz')) {
-            throw new \Exception(Text::_('JERROR_ALERTNOAUTHOR'), 403);
-        }
-
-        $input = json_decode(file_get_contents('php://input'), true);
-
-        $quiz_id = $input['quizId'];
-        $question_ids = $input['questionIds'];
-
-        $model = new YaquizModel();
-        if($model->changeQuestionOrder($quiz_id, $question_ids)){
-            $response = 'success';
-            echo json_encode($response);
-        }
-        else{
-            $response = 'fail';
-            echo json_encode($response);
-        }
-
-
-
+        echo 'something';
     }
 
 
